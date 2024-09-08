11906bf0c0ac7dabf8d52ad710fef38f417ff2c7
Philipp Rich <skorphil@gmail.com>
2024-09-08 13:03:28 +0400

    upd this

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 import path from "path"; // Use ES module syntax for importing modules
 
 export default {
-  entry: "./src/getExample.js", // Entry point for your application
+  entry: "./src/index.js",
   output: {
-    filename: "bundle.js", // Output file name
-    path: path.resolve(process.cwd(), "dist"), // Output directory
-    library: "YourLibraryName", // Name of the exposed global variable
-    libraryTarget: "umd", // Universal module definition for browser and Node.js environments
+    filename: "bundle.js",
+    path: path.resolve(process.cwd(), "dist"),
+    library: "YourLibraryName", // This defines the global object name
+    libraryTarget: "umd", // UMD exposes the library for all environments
+    globalObject: "this", // To ensure compatibility with web workers and other environments
   },
-  mode: "production", // Production mode for optimizations
+  mode: "production",
 };
